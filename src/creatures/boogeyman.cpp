694fda1971772787d03ae834fb505693fd7c060d
miriamdaily <miriamthegreat123@gmail.com>
2026-04-15 16:40:51 -0700

    Creature boogeyman factory

---
+++ src/creatures/boogeyman.cpp
@@ -0,0 +1,13 @@
+// Miriam and PJ
+#include "boogeyman.h"
+#include <iostream>
+#include <string>
+using namespace std;
+Creature makeBoogeyman () {
+    public:
+    string name = "Boogeyman";
+    int health = 120;
+    int damage = 15;
+
+    return Creature("boogeyman", 120, 15);
+};
---
+++ src/creatures/boogeyman.h
@@ -0,0 +1,4 @@
+#ifndef BOOGEYMAN_H
+#define BOOGEYMAN_H
+
+#endif
